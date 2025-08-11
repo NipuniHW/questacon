3ae1ac814073df29f9b9b54a24aaf3aa8fb5d49d
BuddhiGamage <buddhigamage217@gmail.com>
2025-08-11 15:25:47 +1000

    created another branch without looping animations.

--- app.py
+++ app.py
@@ -36,9 +36,9 @@ if 'pepper' not in st.session_state:
     # st.session_state.ip='127.0.0.1'
     # port=39673
     # st.session_state.ip='10.0.0.244'
-    st.session_state.ip='172.16.35.227' # questacon ip
+    # st.session_state.ip='172.16.35.227' # questacon ip
     # st.session_state.ip='172.20.10.4'
-    # st.session_state.ip='192.168.254.40'
+    st.session_state.ip='10.230.227.40'
     port=9559
     st.session_state.session = st.session_state.pepper.connect(st.session_state.ip, port)
 
